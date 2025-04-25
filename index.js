fb65740ae86b0708cd096ee65fac38458bea419b
manjunath265 <manjunathmanjunathreddy580@gmail.com>
2025-04-24 19:55:05 -0500

    Initial backend setup for portfolio

---
+++ index.js
@@ -0,0 +1,21 @@
+const express = require("express");
+const path = require("path");
+const cors = require("cors");
+
+const app = express();
+const PORT = process.env.PORT || 5000;
+
+app.use(cors()); // Allow frontend to access this server
+
+// Serve the resume file
+app.get("/resume", (req, res) => {
+  const filePath = path.join(__dirname, "resume.pdf");
+  res.sendFile(filePath);
+});
+
+app.listen(PORT, () => {
+  console.log(`✅ Backend running on http://localhost:${PORT}`);
+});
+
+
+app.get("")
